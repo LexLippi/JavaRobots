156fac35883ff930da2807219ec70db67a95ac61
lexlippi <alekseilipatkin@mail.ru>
2020-05-27 16:16:52 +0500

    add accelerating and slowing filter

---
+++ src/sound/algorithms/AcceleratingFilter.java
@@ -0,0 +1,36 @@
+package sound.algorithms;
+
+import sound.Song;
+import javax.sound.sampled.AudioFormat;
+
+public class AcceleratingFilter extends SoundFilter {
+    private int accelerator;
+
+    public AcceleratingFilter(int accelerator) {
+        this.accelerator = accelerator;
+    }
+
+    @Override
+    byte[] getFilteredData(byte[] data) {
+        var result = new byte[data.length / accelerator];
+        for (var i = 0; i < result.length; i += 2) {
+            setSample(result, i, getSample(data, accelerator * i));
+        }
+        return result;
+    }
+
+    @Override
+    Song getSongFromBytes(byte[] bytes, AudioFormat format, String songName) {
+        return new Song(bytes, format, "Accelerating " + songName);
+    }
+
+//    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+//        var filename = "C:\\Users\\aleks\\Desktop\\Study\\Java\\Robots\\resources\\songs\\TestSample.wav";
+//        var song = new Song(new File(filename).toURL());
+//        var filter = new AcceleratingFilter();
+//        var newSong = filter.transformateSong(song);
+//        var player = new MusicPlayer(new Song[] {newSong});
+//        player.play();
+//        Thread.sleep(50000);
+//    }
+}
---
+++ src/sound/algorithms/SlowingFilter.java
@@ -0,0 +1,26 @@
+package sound.algorithms;
+
+import sound.Song;
+import javax.sound.sampled.AudioFormat;
+
+public class SlowingFilter extends SoundFilter {
+    public int moderator;
+
+    public SlowingFilter(int moderator) {
+        this.moderator = moderator;
+    }
+
+    @Override
+    byte[] getFilteredData(byte[] data) {
+        var result = new byte[data.length * moderator];
+        for (var i = 0; i < result.length; i += 2 * moderator) {
+            setSample(result, i, getSample(data, i / moderator));
+        }
+        return result;
+    }
+
+    @Override
+    Song getSongFromBytes(byte[] bytes, AudioFormat format, String songName) {
+        return new Song(bytes, format, "Slowing " + songName);
+    }
+}
